888945fba2f5fdbaa83f821c37b376517440fffe
xcutiepatootie <mackycalabia@gmail.com>
2024-02-13 10:09:31 +0800

    feat: use zod for type checking

--- components/Forms/SigninForm.tsx
+++ components/Forms/SigninForm.tsx
@@ -1,28 +1,59 @@
 "use client"
-import { signIn } from 'next-auth/react'
-import React, { FormEvent, useRef, useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { FormFields, FormSchema } from '@/types/types';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { fromZodError } from 'zod-validation-error';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { toast } from '../ui/use-toast';
+
 
 
 const SigininForm = () => {
-    const emailRef = useRef<HTMLInputElement>(null)
-    const passwordRef = useRef<HTMLInputElement>(null)
-    const [userRole, setUserRole] = useState<'faculty' | 'student' | null>(null);
+    const router = useRouter()
 
-    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
+    const { register, handleSubmit, setError,
+        formState: { errors, isSubmitting }
+    } = useForm<FormFields>({ resolver: zodResolver(FormSchema) });
+
+
+    const onSubmit: SubmitHandler<FormFields> = async (data) => {
+        console.log(data);
+
         const result = await signIn('credentials', {
-            email: emailRef.current?.value,
-            password: passwordRef.current?.value,
-            userRole: userRole,
-
-            callbackUrl: '/dashboard'
+            email: data.email,
+            password: data.password,
+            redirect: false,
         })
-    }
 
+        if (result?.status === 200) {
+            router.push('/dashboard');
+            toast({
+                title: 'Redirecting...',
+                description: 'Login Successful'
+            })
+        }
+
+        if (result?.status === 401) {
+            router.push('/dashboard');
+            toast({
+                title: 'Login Failed',
+                description: 'Invalid Email or Password',
+                variant: 'destructive'
+            })
+        }
+        console.log(result)
+
+
+
+        const res = FormSchema.safeParse(data);
+        if (!res.success) {
+            console.log(fromZodError(res.error));
+        }
+    }
     return (
         <div className="flex justify-center items-center h-screen">
-
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-4 border-b-2 border-zinc-300" htmlFor="SigninTag">
                     Sign-In
                 </label>
@@ -30,58 +61,60 @@ const SigininForm = () => {
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                         Email
                     </label>
-                    <input
+                    <input {...register('email')}
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="email"
-                        type="email"
+
                         placeholder="Enter your email"
-                        ref={emailRef}
-                        required
                     />
+                    {errors.email && (<div className='text-red-500'>{errors.email.message}</div>)}
                 </div>
                 <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                         Password
                     </label>
-                    <input
+                    <input {...register('password', { required: true })}
                         className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                         id="password"
                         type="password"
                         placeholder="Enter your password"
-                        ref={passwordRef}
-                        required
-                    />
 
+
+                    />
+                    {errors.password && (<div className='text-red-500'>{errors.password.message}</div>)}
                 </div>
-                <div className="mb-4">
+                {/*  <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="userType">
                         Role
                     </label>
-                    <div className="flex items-center">
-                        <label className="mr-4">
+                    <div className="flex items-center justify-around">
+                        <div>
                             <input
+                                {...register('role', { required: true })}
                                 type="radio"
-                                name="userType"
+                                id="faculty"
+                                name="role"
                                 value="faculty"
-                                onChange={() => { setUserRole('faculty') }}
-                                required
                             />
                             <span className="ml-2">Faculty</span>
-                        </label>
-                        <label>
+                        </div>
+                        <div>
                             <input
+                                {...register('role', { required: true })}
                                 type="radio"
-                                name="userType"
+                                id="student"
+                                name="role"
                                 value="student"
-                                onChange={() => { setUserRole('student') }}
-                                required
                             />
                             <span className="ml-2">Student</span>
-                        </label>
+
+                        </div>
                     </div>
-                </div>
+                    {errors.role && (<div className='text-red-500'>{errors.role.message}</div>)}
+                </div> */}
                 <div className="flex items-center justify-between">
                     <button
+                        disabled={isSubmitting}
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-transform transform hover:scale-105"
                         type="submit"
                     >
--- components/Forms/SignupForm.tsx
+++ components/Forms/SignupForm.tsx
@@ -1,59 +1,45 @@
 "use client"
-import axios from 'axios'
-import React, { SyntheticEvent, useRef, useState } from 'react'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+
+import { SignUpFormFields, SignUpFormSchema } from '@/types/types';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { fromZodError } from 'zod-validation-error';
+import { createUser } from '@/lib/server_actions/actions';
+import { useToast } from '../ui/use-toast';
+
+
 
 const SignupForm = () => {
-    const emailRef = useRef<HTMLInputElement>(null)
-    const passwordRef = useRef<HTMLInputElement>(null)
-    const usernameRef = useRef<HTMLInputElement>(null)
-    const nameRef = useRef<HTMLInputElement>(null)
-    const [userRole, setUserRole] = useState<'faculty' | 'student' | null>(null);
+    const { toast } = useToast();
+    const { register, handleSubmit, setError,
+        formState: { errors, isSubmitting }
+    } = useForm<SignUpFormFields>({ resolver: zodResolver(SignUpFormSchema) });
 
+    const onSubmit: SubmitHandler<SignUpFormFields> = async (data) => {
+        console.log(data)
+        const signUpUser = await createUser(data);
 
-    const handleSubmit = async (event: SyntheticEvent) => {
-        event.preventDefault()
+        if (signUpUser) {
+            toast({
+                className: 'bg-green-600 text-neutral-100',
+                title: 'SmartScore',
+                description: 'Successfully Created the User.'
+            })
+            console.log(signUpUser)
 
-        console.log(userRole)
-        try {
-            const response = await axios.post('/api/signup-user', {
-                name: nameRef.current?.value,
-                username: usernameRef.current?.value,
-                email: emailRef.current?.value,
-                password: passwordRef.current?.value,
-                role: userRole
-            });
-
-            console.log(response.data);
-
-
-            if (response.status === 200) {
-                toast.success(response.statusText)
-            }
-            if (response.status === 409) {
-                toast.warning("Username or Email already existing")
-            }
-        } catch (error) {
-            console.log(error);
         }
 
+
+        const res = SignUpFormSchema.safeParse(data);
+        if (!res.success) {
+            console.log(fromZodError(res.error));
+        }
     }
     return (
 
         <div className="flex justify-center items-center h-screen">
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="colored" />
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" >
                 <label className="block text-gray-700 text-sm font-bold mb-4 border-b-2 border-zinc-300" htmlFor="SignupTag">
                     Sign-Up
                 </label>
@@ -62,13 +48,14 @@ const SignupForm = () => {
                         Name
                     </label>
                     <input
+                        {...register('name', { required: true })}
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="name"
                         type="text"
                         placeholder="Enter your Name"
-                        ref={nameRef}
-                        required
                     />
+                    {errors.name && (<div className='text-red-500'>{errors.name.message}</div>)}
+
                 </div>
 
                 <div className="mb-4">
@@ -76,13 +63,15 @@ const SignupForm = () => {
                         Username
                     </label>
                     <input
+                        {...register('username', { required: true })}
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="username"
                         type="text"
                         placeholder="Enter your username"
-                        ref={usernameRef}
-                        required
+
                     />
+                    {errors.username && (<div className='text-red-500'>{errors.username.message}</div>)}
+
                 </div>
 
                 <div className="mb-4">
@@ -90,55 +79,64 @@ const SignupForm = () => {
                         Email
                     </label>
                     <input
+                        {...register('email', { required: true })}
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="email"
                         type="email"
                         placeholder="Enter your email"
-                        ref={emailRef}
-                        required
+
                     />
+                    {errors.email && (<div className='text-red-500'>{errors.email.message}</div>)}
+
                 </div>
 
                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="userType">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Role
                     </label>
-                    <div className="flex items-center">
-                        <label className="mr-4">
+                    <div className="flex items-center justify-around">
+                        <div>
                             <input
+                                {...register('role', { required: true })}
                                 type="radio"
-                                name="userType"
+                                id="faculty"
+                                name="role"
                                 value="faculty"
-                                onChange={() => { setUserRole('faculty') }}
-                                required
                             />
                             <span className="ml-2">Faculty</span>
-                        </label>
-                        <label>
+                        </div>
+                        <div>
                             <input
+                                {...register('role', { required: true })}
                                 type="radio"
-                                name="userType"
+                                id="student"
+                                name="role"
                                 value="student"
-                                onChange={() => { setUserRole('student') }}
-                                required
                             />
                             <span className="ml-2">Student</span>
-                        </label>
+
+                        </div>
                     </div>
+                    {errors.role && (<div className='text-red-500'>Please Select A Role</div>)}
+
                 </div>
                 <div className="mb-6">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                         Password
                     </label>
                     <input
-                        className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        {...register('password', { required: true })}
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                         id="password"
                         type="password"
                         placeholder="Enter your password"
-                        ref={passwordRef}
-                        required
-                    />
 
+                    />
+                    {errors.password && (<div className='text-red-500 text-xs'>
+                        Password must be at least 8 characters long
+                        <br /> Password must contain at least one uppercase letter
+                        <br /> Password must contain at least one symbol
+                    </div>)}
 
 
                 </div>
