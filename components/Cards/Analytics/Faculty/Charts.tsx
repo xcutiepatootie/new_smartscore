814997961e686dec1c3edd01e383bdf4f4d44d39
xcutiepatootie <mackycalabia@gmail.com>
2024-03-24 17:48:22 +0800

    feat: added buttons for selecting distance and xy values

--- components/Cards/Analytics/Faculty/Charts.tsx
+++ components/Cards/Analytics/Faculty/Charts.tsx
@@ -1,15 +1,32 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Quiz_section_Popover from "../../Dashboard/Faculty/Quiz_Section/Quiz_section_Popover/Quiz_section_Popover";
 import { getClusterChart } from "@/lib/server_actions/actions";
+import { Card, CardContent } from "@/components/ui/card";
+import DistancePicker from "./DropdownMenu/DistancePicker";
+import ValuesPicker from "./DropdownMenu/ValuesPicker";
+import { Label } from "@/components/ui/label";
 
 const Charts = ({ quizzes }: any) => {
   console.log(quizzes);
 
   const [selectedQuiz, setSelectedQuiz] = useState<string>("");
-  const [selectedQuizId, setSelectedQuizId] = useState<string>(""); // State to store the selected quiz id
+  const [selectedQuizId, setSelectedQuizId] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
   const [data, setData] = useState("");
+  const [xvalue, setXValue] = useState<string>("");
+  const [yvalue, setYValue] = useState<string>("");
+  const [selectedDistanceMethod, setSelectedDistanceMethod] =
+    useState<string>("");
+
+  // Memoize event handler functions using useCallback
+  const handleSetXValue = useCallback((value: string) => {
+    setXValue(value);
+  }, []);
+
+  const handleSetYValue = useCallback((value: string) => {
+    setYValue(value);
+  }, []);
 
   useEffect(() => {
     // Find the selected quiz and set its id
@@ -34,7 +51,7 @@ const Charts = ({ quizzes }: any) => {
 
       setTimeout(() => {
         fetchChart().then((fetchedData: any) => {
-          console.log("Image to ha", fetchedData);
+          //   console.log("Image to ha", fetchedData);
           setData(fetchedData);
           setLoading(false);
         });
@@ -43,13 +60,41 @@ const Charts = ({ quizzes }: any) => {
   }, [selectedQuizId]);
 
   console.log(selectedQuiz, selectedQuizId);
+  console.log(xvalue);
+  console.log(yvalue);
+  console.log(selectedDistanceMethod);
   return (
     <div>
       <Quiz_section_Popover
         quizzes={quizzes}
         setSelectedQuiz={setSelectedQuiz}
       />
-      {data && <img src={`data:image/png;base64,${data}`} alt="Base64 Image" />}
+      <div className="grid grid-cols-2">
+        <Card className="w-auto p-2">
+          <CardContent>
+            {data && (
+              <>
+                <div className="flex flex-row space-x-4">
+                  <DistancePicker
+                    setSelectedDistanceMethod={setSelectedDistanceMethod}
+                  />
+                  <div>
+                    <Label>X Value: </Label>
+                    <ValuesPicker setSelectedValue={handleSetXValue} />
+                  </div>
+                  <div>
+                    <Label>Y Value: </Label>
+                    <ValuesPicker setSelectedValue={handleSetYValue} />
+                  </div>
+                </div>
+
+                <img src={`data:image/png;base64,${data}`} alt="Base64 Image" />
+              </>
+            )}
+          </CardContent>
+        </Card>
+        <div></div>
+      </div>
 
       {/* <div>{imageSrc && <img src={imageSrc} alt="API Image" />}</div> */}
     </div>
---
+++ components/Cards/Analytics/Faculty/DropdownMenu/ValuesPicker.tsx
@@ -0,0 +1,75 @@
+import { Check, ChevronsUpDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { SetStateAction, useState } from "react";
+import { xy_values } from "./Attribute_Values/Attributes";
+
+const ValuesPicker = ({
+  setSelectedValue,
+}: {
+  setSelectedValue: (value: string) => void;
+}) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [value, setValue] = useState<string>("");
+
+  const handleSelectValue = (currentValue: SetStateAction<string>) => {
+    const newValue = currentValue === value ? "" : currentValue;
+
+    setValue(newValue);
+    setOpen(false);
+    setSelectedValue(newValue as string);
+  };
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-auto justify-between"
+        >
+          {value
+            ? xy_values.find((dMethod) => dMethod.value === value)?.label
+            : "Select Attribute..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandGroup>
+            {xy_values.map((dMethod) => (
+              <CommandItem
+                key={dMethod.value}
+                value={dMethod.value}
+                onSelect={handleSelectValue}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    value === dMethod.value ? "opacity-100" : "opacity-0"
+                  )}
+                />
+                {dMethod.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default ValuesPicker;
