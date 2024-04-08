e10f8338765e8159d0afd4229ec8cb97fdb60126
xcutiepatootie <mackycalabia@gmail.com>
2024-04-08 22:05:02 +0800

    fix: login

--- lib/auth.ts
+++ lib/auth.ts
@@ -48,7 +48,7 @@ export const config: NextAuthOptions = {
         //const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
         try {
           const response = await axios.post(
-            "http://localhost:3000/api/signin-user",
+            `${process.env.API_TS_URL}/api/signin-user`,
             {
               email: credentials?.email,
               password: credentials?.password,
