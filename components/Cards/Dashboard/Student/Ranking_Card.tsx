1493bca2acd0335c68823b43bf728ca479fabfd8
xcutiepatootie <mackycalabia@gmail.com>
2024-04-08 19:10:12 +0800

    refactor: separator

--- components/Cards/Dashboard/Student/Ranking_Card.tsx
+++ components/Cards/Dashboard/Student/Ranking_Card.tsx
@@ -194,8 +194,8 @@ const Ranking_Card = ({
             setSelectedQuiz={setSelectedQuiz}
           />
         </CardHeader>
-        <Separator className="mx-4 my-2 h-1 bg-amber-200" />
         <CardContent className="">
+          <Separator className=" my-2 h-1 bg-amber-200" />
           {/* {sample.map((items, index) => (
             <div
               className="m-4 flex w-full flex-row items-center justify-center gap-2 p-4"
